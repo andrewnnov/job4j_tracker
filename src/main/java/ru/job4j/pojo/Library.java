999201dc587dac74cfd55c739010ba7d0e0b20b1
andrewnnov <andrewnnov@yandex.ru>
2022-05-02 12:43:55 +0300

    3. Массивы и модели. [#174892]

---
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -0,0 +1,45 @@
+package ru.job4j.pojo;
+
+public class Library {
+
+    public static void main(String[] args) {
+        Book bookOne = new Book("Clean code", 100);
+        Book bookTwo = new Book("Notion", 150);
+        Book bookThree = new Book("Dirty code", 600);
+        Book bookFour = new Book("NPM", 400);
+
+        Book[] books = new Book[4];
+        books[0] = bookOne;
+        books[1] = bookTwo;
+        books[2] = bookThree;
+        books[3] = bookFour;
+
+        for (int index = 0; index < books.length; index++) {
+
+            Book bk = books[index];
+            System.out.println(bk.getName() + " - " + bk.getCount());
+        }
+        System.out.println();
+
+        Book temp = books[0];
+        books[0] = books[3];
+        books[3] = temp;
+
+        System.out.println("Replace book 0 and 3");
+        for (int index = 0; index < books.length; index++) {
+            Book bk = books[index];
+            System.out.println(bk.getName() + " - " + bk.getCount());
+        }
+        System.out.println();
+
+        System.out.println("Choose only Clean code");
+        for (int index = 0; index < books.length; index++) {
+            Book bk = books[index];
+            if (bk.getName().equals("Clean code")) {
+                System.out.println(bk.getName() + " - " + bk.getCount());
+            }
+
+        }
+
+    }
+}
