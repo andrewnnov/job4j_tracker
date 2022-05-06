cd70f7053020067f3d3bc7bffdcfb8fa70c5c12e
andrewnnov <andrewnnov@yandex.ru>
2022-05-06 07:40:05 +0300

    9. toString [#310018]

---
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,9 @@
+package ru.job4j.tracker;
+
+public class StartUI {
+
+    public static void main(String[] args) {
+        Item item = new Item("Item");
+        System.out.println(item);
+    }
+}
