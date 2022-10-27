109b61ebf2215df549bfe618c5f7f4bc0cbc9f06
andrewnnov <andrewnnov@yandex.ru>
2022-10-27 10:22:33 +0300

    6.9. String - это массив [#504884] FIX

--- src/test/java/ru/job4j/array/JavaNameValidatorTest.java
+++ src/test/java/ru/job4j/array/JavaNameValidatorTest.java
@@ -9,49 +9,41 @@ public class JavaNameValidatorTest {
 
     @Test
     public void whenEmptyNameInvalid() {
-        //assertThat(isNameValid("")).isFalse();
         assertFalse(isNameValid(""));
     }
 
     @Test
     public void whenFullLatValid() {
-        //assertThat(isNameValid("first")).isTrue();
         assertTrue(isNameValid("first"));
     }
 
     @Test
     public void whenFullLatAndSomeUpperCaseLettersValid() {
-        //assertThat(isNameValid("fIRST")).isTrue();
         assertTrue(isNameValid("fIRST"));
     }
 
     @Test
     public void whenLatNumberValid() {
-        //assertThat(isNameValid("first1")).isTrue();
         assertTrue(isNameValid("first1"));
     }
 
     @Test
     public void whenLatWithSpecialSymbolUnderValid() {
-        //assertThat(isNameValid("first_user")).isTrue();
         assertTrue(isNameValid("first_user"));
     }
 
     @Test
     public void whenNumberInValid() {
-        //assertThat(isNameValid("123")).isFalse();
         assertFalse(isNameValid("123"));
     }
 
     @Test
     public void whenFirstLatinUpperCaseIsInvalid() {
-        //assertThat(isNameValid("First")).isFalse();
         assertFalse(isNameValid("First"));
     }
 
     @Test
     public void whenLatNumberAndTwoSpecialSymbolsUnderValid() {
-        //assertThat(isNameValid("fir$t_u$er_1")).isTrue();
         assertTrue(isNameValid("fir$t_u$er_1"));
     }
 
