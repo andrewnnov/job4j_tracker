ac7296fb378caa076eee2fb914ed83f104d9cd11
andrewnnov <andrewnnov@yandex.ru>
2023-01-29 15:18:24 -0300

    create classes Tracker and Item

---
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -0,0 +1,43 @@
+package ru.job4j.tracker;
+
+import java.time.LocalDateTime;
+
+public class Item {
+
+    private int id;
+    private String name;
+    private LocalDateTime created = LocalDateTime.now();
+
+    public Item() {
+
+    }
+
+    public Item(String name) {
+        this.name = name;
+    }
+
+    public Item(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+}
