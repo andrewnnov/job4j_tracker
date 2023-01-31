9b695d1bee9e009bfd2d8efb77bd1d82bdb09414
andrewnnov <andrewnnov@yandex.ru>
2023-01-31 15:57:38 -0300

    delete all package

--- src/main/java/ru/job4j/oop/Airplane.java
+++
@@ -1,8 +0,0 @@
-package ru.job4j.oop;
-
-public class Airplane implements Vehicle {
-    @Override
-    public void move() {
-        System.out.println("Летаю");
-    }
-}
--- src/main/java/ru/job4j/tracker/StartUI.java
+++
@@ -1,9 +0,0 @@
-package ru.job4j.tracker;
-
-public class StartUI {
-
-    public static void main(String[] args) {
-        Item item = new Item("Item");
-        System.out.println(item);
-    }
-}
